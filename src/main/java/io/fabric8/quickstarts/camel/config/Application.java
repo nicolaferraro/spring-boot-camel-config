82ffb1a64e47f41fbc2ebd1e7c06749f0fb65f8f
Nicola Ferraro <ni.ferraro@gmail.com>
2016-12-09 13:04:15 +0100

    Working version

--- src/main/java/io/fabric8/quickstarts/camel/config/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/config/Application.java
@@ -13,7 +13,7 @@
  *  implied.  See the License for the specific language governing
  *  permissions and limitations under the License.
  */
-package io.fabric8.quickstarts.camel.amq;
+package io.fabric8.quickstarts.camel.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
